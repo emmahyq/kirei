3639de8de9bd6d87442f57904b32b044e009f58f
Emma H <emmahuang37@gmail.com>
2021-08-15 05:36:07 -0700

    Add files via upload

---
+++ tips.js
@@ -0,0 +1,64 @@
+data = [
+    {
+        "name": "Niacinamide",
+        "link": "https://en.wikipedia.org/wiki/Nicotinamide",
+        "desc": "Also known as vitamin B3 - niacinamide is great for brightning and hydration as well as personally relieving redness and irritation. It's nice for sensitive skin and cystic acne. As with all things, using too much of it too often can - ironically - cause irritation in the skin, so be cautious!",
+        "rec": "Glossier Super Pure - $28",
+        "reclink": "https://www.glossier.com/products/super-pure",
+        "abv": "nd"
+    }, 
+
+    {
+        "name": "Salicylic Acid",
+        "link": "https://en.wikipedia.org/wiki/Salicylic_acid",
+        "desc": "Salicylic acid is an exfoliant that aims to remove dead skin cells and excess oil. It's good for surface acne like whiteheads and blackheads, and like everything else should be used in moderation. Be careful since it can increase skin sensitivity to sunlight, so I only use it at night before bed.",
+        "rec": "Caudalíe Vinopure Serum - $49",
+        "reclink": "https://us.caudalie.com/344na-vinopure-skin-perfecting-serum.html",
+        "abv": "sa"
+    },
+
+    {
+        "name": "Benzoyl Peroxide",
+        "link": "https://en.wikipedia.org/wiki/Benzoyl_peroxide",
+        "desc": "This antibacterial agent is for killing acne-causing bacteria. Rather than targeting the cause of acne like salicylic acid, benozyl peroxide targets the acne itself. I avoid using them both together because it has caused serious irritation for me in the past. Benozyl peroxide is nice for emergencies, but is very drying and mildly irritating so I only use it occasionally. It might be good for you if your skin is less sensitive.",
+        "rec": "Neutrogena Spot Treatment Gel - $11",
+        "reclink": "https://www.target.com/p/neutrogena-rapid-clear-stubborn-acne-medicine-spot-treatment-gel-1oz/-/A-16600132#lnk=sametab",
+        "abv": "bx"
+    },
+
+    {
+        "name": "Glycolic Acid",
+        "link": "https://en.wikipedia.org/wiki/Glycolic_acid",
+        "desc": "Glycolic acid is an AHA (alpha-hydroxy acid) derived from sugarcane that helps remove dead skin cells and has been found to stimulate collagen production. It's nice for the red cystic acne that lies deep under the skin. Of course, it's still an acid so too much can cause redness, irritation, and peeling. I try using it every other day or every three days.",
+        "rec": "Glossier Solution - $24",
+        "reclink": "https://www.glossier.com/products/solution",
+        "abv": "gy"
+    },
+
+    {
+        "name": "Vitamin C",
+        "link": "https://en.wikipedia.org/wiki/Vitamin_C",
+        "desc": "Although vitamin C definitely has other benefits (healing sun damage, helping with skin texture, etc) I find it helps a lot with unsightly acne scars that are left behind afterwards. Its antioxidant properties help with the skin's natural regeneration process and repairs damaged skin cells.",
+        "rec": "Klairs Vitamin C Drop - $23",
+        "reclink": "https://sokoglam.com/collections/vitamin-c/products/klairs-freshly-juiced-vitamin-c",
+        "abv": "vc"
+    },
+
+    {
+        "name": "Hyaluronic Acid",
+        "link": "https://en.wikipedia.org/wiki/Hyaluronic_acid",
+        "desc": "While a hydrating ingredient (which is what this does) might seem counterintuitive for acne, it turns out that too-dry skin can lead to even more sebum (oil) production, since your skin thinks it's too dry. This causes even more acne in a vicious cycle. Hyaluronic acid is alternative to oils to help with hydrating your skin without clogging your pores.",
+        "rec": "The Ordinary Hyaluronic Acid 2% + B5 - $7",
+        "reclink": "https://www.ulta.com/p/hyaluronic-acid-2-b5-pimprod2007107",
+        "abv": "hc"
+    },
+
+    {
+        "name": "Hydrocolloid Patches",
+        "link": "https://en.wikipedia.org/wiki/Hydrocolloid_dressing",
+        "desc": "This... isn't a serum or topical cream, but actually a bandage-like sticker that goes on acne. It absorbs excess fluid over a couple hours, like the oil and pus inside acne, and helps speed up healing time. They look extremely gross when they work properly, but are great for emergencies. Bonus: they help stop you from picking at acne and opening wounds!",
+        "rec": "Rael Invisible Spot Cover - $12",
+        "reclink": "https://www.getrael.com/products/miracle-patch",
+        "abv": "hd"
+    }
+]
